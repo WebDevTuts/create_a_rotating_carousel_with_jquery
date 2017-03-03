7fada5a457d7411490c194ef27cdc476d40e9ef6
edwinchen85 <edwinchen85@gmail.com>
2017-03-03 14:44:17 +0800

    Adding images to the carousel container and spin up roundabout plugin on the carousel.

--- includes/carousel.js
+++ includes/carousel.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+
+$(document).ready(function() {
+
+  $('.carousel_data .carousel_item').each(function() {
+
+    $('#carousel').append( $(this).find('.image').html() )
+
+  });
+
+  createCarousel();
+
+});
+
+function createCarousel() {
+
+  $('div#carousel').roundabout({
+    childSelector: 'img',
+    tilt: -4.5,
+    minOpacity: 1,
+    minScale: .45,
+    duration: 1200
+  });
+
+}
